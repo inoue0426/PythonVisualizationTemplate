8b081e09475151ef11917b3b7d359caac103dd44
inoue0426 <inoue0426@users.noreply.github.com>
2024-08-03 16:22:11 +0000

    style: Apply auto-formatting

--- visualization.py
+++ visualization.py
@@ -1,18 +1,38 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 # https://sashamaps.net/docs/resources/20-colors/
-colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
-          '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
-          '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
-          '#000075', '#808080', '#ffffff', '#000000']
+colors = [
+    "#e6194b",
+    "#3cb44b",
+    "#ffe119",
+    "#4363d8",
+    "#f58231",
+    "#911eb4",
+    "#46f0f0",
+    "#f032e6",
+    "#bcf60c",
+    "#fabebe",
+    "#008080",
+    "#e6beff",
+    "#9a6324",
+    "#fffac8",
+    "#800000",
+    "#aaffc3",
+    "#808000",
+    "#ffd8b1",
+    "#000075",
+    "#808080",
+    "#ffffff",
+    "#000000",
+]
 
 plt.rcParams["font.family"] = "Arial"
 plt.rcParams["pdf.fonttype"] = 42
 cmap = mcolors.ListedColormap(colors)
 
-sns.set_style("whitegrid") 
+sns.set_style("whitegrid")
 sns.set_palette(sns.color_palette(colors))
